7c4eda4237dc128e000d63a714132fe69251a9bd
alireza kasirzare <alirezakasir@yahoo.com>
2023-03-21 07:23:11 +0330

    add tailwind and tailwind-styled-components packages to project

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,15 @@
+import tw from 'tailwind-styled-components';
+
+const PageTitle = tw.div`
+  bg-blue-500 text-white
+  p-3 m-3
+  rounded-md
+  text-center 
+  font-bold font-xl
+`;
+
 function HomePage() {
-  return <div>the Home page</div>;
+  return <PageTitle>صفحه خانه</PageTitle>;
 }
 
 export default HomePage;
